0e949e7e1d4fa0a0d94fec71d5d2050302bdd6fd
CesarCedanoV <CesarCedanoV@gmail.com>
2019-05-24 23:14:49 -0400

    Updated the app to serve the client side (react) files from the server side (express)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 // Create the Express server
 const app = express();
@@ -13,10 +14,19 @@ app.use(bodyParser.json());
 // allow request from different origin
 app.use(cors());
 
+// Serve static files from the client side
+app.use(express.static(path.join(__dirname, "client/build")));
+
 // A simple endpoint to check
 // if the server is running
 app.get("/", function(req, res) {
   res.send("Human Resources API - Health Check");
+});
+
+// This endpoint will "catch all" the requests that
+// doesn't match one above and send back client index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "client/build/index.html"));
 });
 
 // Set the PORT from an env var or assign a default one
