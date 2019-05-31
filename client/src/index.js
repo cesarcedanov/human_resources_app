bd9290da0e64e4f9358402415976d564396c9a9d
CesarCedanoV <CesarCedanoV@gmail.com>
2019-05-30 21:05:58 -0400

    Added the ApolloClient 2.0 options

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,33 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "react-apollo";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// REQUIRED FOR APOLLO CLIENT 2.0
+const cache = new InMemoryCache({
+  dataIdFromObject: o => o.id
+});
+
+const link = new HttpLink({
+  uri: "/graphql"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
