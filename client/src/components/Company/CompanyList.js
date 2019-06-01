c37557744876946c7781081e60a415fc41b73031
CesarCedanoV <CesarCedanoV@gmail.com>
2019-06-01 02:18:26 -0400

    Fixed some details in the Company components

--- client/src/components/Company/CompanyList.js
+++ client/src/components/Company/CompanyList.js
@@ -1,11 +1,48 @@
 import React from "react";
+import gql from "graphql-tag";
 import { graphql } from "react-apollo";
-import { List, Card, Icon } from "antd";
+import { List, Card, Icon, Button, Row, Col, Typography } from "antd";
 
 import CompanyCreate from "./CompanyCreate";
 import fetchCompanies from "../../queries/fetchCompanies";
 
 class CompanyList extends React.Component {
+  //Modal Button
+  state = {
+    visible: false
+  };
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, formValues) => {
+      if (err) {
+        return;
+      }
+
+      this.props.mutate({
+        variables: {
+          ...formValues,
+          founded: formValues.founded.format("YYYY-MM")
+        },
+        refetchQueries: [{ query: fetchCompanies }]
+      });
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  };
+
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+
   getActionBtn = (socialType, term) => {
     return (
       <Icon
@@ -35,7 +72,24 @@ class CompanyList extends React.Component {
   render() {
     return (
       <>
-        <CompanyCreate />
+        <Button
+          style={{
+            marginBottom: 25
+          }}
+          onClick={this.showModal}
+          type="primary"
+          shape="round"
+          icon="plus"
+          size="default"
+        >
+          Add Company
+        </Button>
+        <CompanyCreate
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
         <List
           grid={{
             gutter: 16,
@@ -51,10 +105,23 @@ class CompanyList extends React.Component {
             <List.Item>
               <Card
                 hoverable
-                title={company.name}
+                title={
+                  <div>
+                    <Row>
+                      <Col span={16}>{company.name}</Col>
+                      <Col span={6} offset={2}>
+                        <Typography.Text type="secondary">
+                          {company.founded}
+                        </Typography.Text>
+                      </Col>
+                    </Row>
+                  </div>
+                }
                 actions={this.renderActions(company)}
               >
-                {company.description}
+                <Typography.Paragraph ellipsis={{ rows: 3, expandable: true }}>
+                  {company.description}
+                </Typography.Paragraph>
                 {company.founded}
               </Card>
             </List.Item>
@@ -65,4 +132,29 @@ class CompanyList extends React.Component {
   }
 }
 
-export default graphql(fetchCompanies)(CompanyList);
+const mutation = gql`
+  mutation AddCompany(
+    $name: String
+    $description: String
+    $founded: String
+    $email: String
+    $phone: String
+    $instagram: String
+    $twitter: String
+  ) {
+    addCompany(
+      name: $name
+      description: $description
+      founded: $founded
+      email: $email
+      phone: $phone
+      instagram: $instagram
+      twitter: $twitter
+    ) {
+      id
+      name
+    }
+  }
+`;
+
+export default graphql(fetchCompanies)(graphql(mutation)(CompanyList));
